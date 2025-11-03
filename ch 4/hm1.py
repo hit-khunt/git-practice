7f7e345ba763627eb0595eb09687cbfa4373b159
Hit <hitkkhunt@gmail.com>
2025-11-03 15:32:25 +0530

    add ch 4 and ch 5 programs

---
+++ ch 4/hm1.py
@@ -0,0 +1,11 @@
+m1 = int(input("Enter Marks Here : "))
+m2 = int(input("Enter Marks Here : "))
+m3 = int(input("Enter Marks Here : "))
+m4 = int(input("Enter Marks Here : "))
+m5 = int(input("Enter Marks Here : "))
+m6 = int(input("Enter Marks Here : "))
+
+marks = [m1,m2,m3,m4,m5,m6]
+
+marks.sort()
+print(marks)
---
+++ ch 5/new.py
@@ -0,0 +1,14 @@
+# Dictionary {}
+marks = {
+    "Hit" : 99,
+    "Mihir" : 67,
+    "Yash" : 56
+}
+
+print(marks)
+print(marks["Hit"])
+
+print(marks.items())
+print(marks.keys())
+print(marks.values())
+
